ce467c7b69df27559a69dfbb678da28f7386979f
khaledaakter <khaledanahar43@gmail.com>
2023-05-16 23:33:37 +0600

    first commit

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = ({cart}) => {
+
+    console.log(cart);
+    let total = 0;
+    let totalShipping = 0;
+    let tex = 0;
+    let grandTotal = 0;
+    for(const product of cart) {
+        total = total + product.price;
+        totalShipping = totalShipping + product.shipping;
+        tex = total*7/100;
+        grandTotal = total + totalShipping + tex + grandTotal;
+    }
+
+    return (
+        <div>
+            <div className="cart">
+                <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping Charge: ${totalShipping}</p>
+                <p>Tax: ${tex}</p>
+                <p className='grand-total'>Grand Total: ${grandTotal}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Cart;
